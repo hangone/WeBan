72a54c06c73147ef0aaa2e8203d27061a604e9ea
hangone <56105779+hangone@users.noreply.github.com>
2025-05-26 01:24:33 +0800

    feat: 添加自动考试

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
+import json
 import os
 import time
 import webbrowser
+from random import randint
 from typing import Any, Dict, Optional, TYPE_CHECKING, Union
 from urllib.parse import parse_qs, urlparse
 
@@ -20,6 +22,20 @@ class WeBanClient:
         self.api = WeBanAPI(account, password)
         self.study_time = 15
         self.fail = []
+
+    @staticmethod
+    def get_project_type(project_category: int) -> str:
+        """
+        获取项目类型
+        :param project_category: 项目类型 1.新生安全教育 2.安全课程 3.专题学习 4.军事理论 9.实验室
+        :return: 项目类型字符串
+        """
+        if project_category == 3:
+            return "special"
+        elif project_category == 9:
+            return "lab"
+        else:
+            return ""
 
     @staticmethod
     def get_ocr_instance(_cache: Dict[str, Any] = {"ocr": None}) -> Optional[Union["DdddOcr", None]]:
@@ -70,7 +86,9 @@ class WeBanClient:
         progress = self.api.show_progress(user_project_id)
         if progress.get("code", "-1") == "0":
             progress = progress.get("data", {})
-            logger.info(f"{project_prefix} 总进度：{progress.get('requiredFinishedNum', 0)}/{progress.get('requiredNum', 0)}")
+            required_finished_num = progress["requiredFinishedNum"]
+            required_num = progress["requiredNum"]
+            logger.info(f"{project_prefix} 总进度：{required_finished_num}/{required_num}，预计剩余时间：{self.study_time*(required_num-required_finished_num)} 秒")
         return progress
 
     def login(self) -> Dict | None:
@@ -165,5 +183,177 @@ class WeBanClient:
         else:
             logger.success("所有课程学习完成")
 
-    def run_exam(self, use_time: int = 2000):
-        return
+    def run_exam(self, use_time: int = 600):
+        # 加载题库
+        answers_list = []
+        answers_json = {}
+
+        with open("answer/answer.json", encoding="utf-8") as f:
+            for title, options in json.load(f).items():
+                answers_list.append(title)
+                correct_answers = [answer["content"] for answer in options.get("optionList", []) if answer.get("isCorrect") == 1]
+                if correct_answers:
+                    answers_json[title] = correct_answers
+
+        # 获取项目
+        projects = self.api.list_my_project()
+        if projects.get("code") != "0":
+            logger.error(f"获取考试列表失败：{projects}")
+            return
+
+        projects = projects.get("data", [])
+        for project in projects:
+            if project["finished"] == 1:
+                logger.success(f"考试项目 {project['projectName']} 已完成")
+                continue
+
+            logger.info(f"开始考试项目 {project['projectName']}")
+            user_project_id = project["userProjectId"]
+
+            # 获取考试计划
+            exam_plans = self.api.exam_list_plan(user_project_id)
+            if exam_plans.get("code", -1) != "0":
+                logger.error(f"获取考试计划失败：{exam_plans}")
+                return
+            exam_plans = exam_plans["data"]
+            for plan in exam_plans:
+                user_exam_plan_id = plan["id"]
+                exam_plan_id = plan["examPlanId"]
+                # 是否存在完成的考试记录
+                before_paper = self.api.exam_before_paper(plan["id"])
+                if before_paper.get("code") != "0":
+                    logger.error(f"获取考试记录失败：{before_paper}")
+                before_paper = before_paper.get("data", {})
+                if before_paper.get("isExistedNotSubmit"):
+                    logger.warning(f"存在未提交的考试数据，继续将清除未提交数据（Y/n）:")
+                    if input().lower() == "n":
+                        logger.error("用户取消")
+                        return
+
+                # 预请求
+                prepare_paper = self.api.exam_prepare_paper(user_exam_plan_id)
+                if prepare_paper.get("code") != "0":
+                    logger.error(f"获取考试信息失败：{prepare_paper}")
+                    continue
+                prepare_paper = prepare_paper.get("data", {})
+                question_num = prepare_paper.get("questionNum", 0)
+                logger.info(f"考试信息：题目数 {question_num}，试卷总分：{prepare_paper.get('paperScore', 0)}，限时 {prepare_paper.get('examTime', 0)} 分钟")
+                per_time = use_time // prepare_paper["answerTime"]
+
+                # 检查验证码
+                is_verified = False
+                retry_limit = 3
+                for i in range(retry_limit + 2):
+                    if i > 0:
+                        logger.info(f"识别失败，正在重试 {i}/{retry_limit+2} 次")
+                    verify_time = self.api.get_timestamp(13, 0)
+                    verify_image = self.api.rand_letter_image(verify_time)
+                    if i < retry_limit and self.ocr:
+                        verify_code = self.ocr.classification(verify_image)
+                        logger.info(f"自动验证码识别结果: {verify_code}")
+                        if len(verify_code) != 4:
+                            logger.info("验证码识别失败，正在重试")
+                            continue
+                    else:
+                        open("verify_code.png", "wb").write(verify_image)
+                        webbrowser.open(f"file://{os.path.abspath("verify_code.png")}")
+                        verify_code = input("请查看 verify_code.png 输入验证码：")
+                    res = self.api.exam_check_verify_code(user_exam_plan_id, verify_code, int(verify_time))
+                    if res.get("code") == "0":
+                        logger.success("验证码正确")
+                        is_verified = True
+                        break
+                    logger.error(f"验证码错误：{res}")
+                if not is_verified:
+                    logger.error("验证码错误，请重新考试")
+                    continue
+                logger.info("验证码正确，开始考试")
+
+                # 获取考试题目
+                exam_paper = self.api.exam_start_paper(user_exam_plan_id)
+                if exam_paper.get("code") != "0":
+                    logger.error(f"获取考试题目失败：{exam_paper}")
+                    continue
+                exam_paper = exam_paper.get("data", {})
+                question_list = exam_paper.get("questionList", [])
+                have_answer = []  # 有答案的题目
+                no_answer = []  # 无答案的题目
+                failed_questions = []  # 答题失败的题目
+
+                for i, question in enumerate(question_list):
+                    if question.get("title") in answers_list:
+                        have_answer.append(question)
+                        continue
+                    no_answer.append(question)
+
+                logger.info(f"题目总数：{question_num}，有答案的题目数：{len(have_answer)}，无答案的题目数：{len(no_answer)}")
+                correct_rate = len(have_answer) / question_num
+                if correct_rate < 0.9:
+                    logger.warning(f"题库正确率 {correct_rate} 少于 90%，是否继续考试？（y/N）")
+                    if input().lower() != "y":
+                        logger.error("用户取消")
+                        continue
+
+                for i, question in enumerate(no_answer):
+                    logger.info(f"[{i}/{len(no_answer)}]题目不在题库中，请手动选择答案")
+                    logger.info(f"题目类型：{question['typeLabel']}，题目标题：{question['title']}")
+                    for j, option in enumerate(question["optionList"]):
+                        logger.info(f"{j + 1}. {option['content']}")
+                    answer = input("请输入答案序号（多个选项用英文逗号分隔，如 1,2,3,4）：")
+                    answers_ids = []
+                    for ans in answer.strip().split(","):
+                        ans = ans.strip()
+                        if ans.isdigit() and 1 <= int(ans) <= len(question["optionList"]):
+                            logger.info(f"选择答案：{ans}，内容：{question['optionList'][int(ans) - 1]['content']}")
+                            answers_ids.append(question["optionList"][int(ans) - 1]["id"])
+                            continue
+                        logger.error(f"无效的答案序号：{ans}，跳过")
+                    logger.info(f"正在提交当前答案")
+                    if not self.record_answer(user_exam_plan_id, question["id"], 7, answers_ids, exam_plan_id):
+                        failed_questions.append(question)
+                        continue
+
+                logger.info(f"手动答题结束，开始答题库中的题目，共 {len(have_answer)} 道题目")
+                for i, question in enumerate(have_answer):
+                    logger.info(f"[{i}/{len(have_answer)}]题目在题库中，开始答题")
+                    logger.info(f"题目类型：{question['typeLabel']}，题目标题：{question['title']}")
+                    for j, option in enumerate(question["optionList"]):
+                        logger.info(f"{j + 1}. {option['content']}")
+                    answers = answers_json[question["title"]]
+                    logger.info(f"题库答案：{', '.join(answers)}")
+                    answers_ids = []
+                    for ans in answers:
+                        for option in question["optionList"]:
+                            if option["content"] == ans:
+                                answers_ids.append(option["id"])
+                                break
+                    if not self.record_answer(user_exam_plan_id, question["id"], per_time, answers_ids, exam_plan_id):
+                        failed_questions.append(question)
+                        continue
+
+                logger.info("完成考试，正在提交答案...")
+                submit_res = self.api.exam_submit_paper(user_exam_plan_id)
+                if submit_res.get("code") != "0":
+                    logger.error(f"提交答案失败：{submit_res}")
+                    continue
+                logger.success(f"答案提交成功，考试完成，成绩：{submit_res.get('data', {}).get('score', 0)} 分")
+
+    def record_answer(self, user_exam_plan_id: str, question_id: str, per_time: int, answers_ids: list, exam_plan_id: str) -> bool:
+        """
+        记录答题
+        :param user_exam_plan_id: 用户考试计划 ID
+        :param question_id: 题目 ID
+        :param per_time: 用时
+        :param answers_ids: 答案 ID 列表
+        :param exam_plan_id: 考试计划 ID
+        :return:
+        """
+        this_time = per_time + randint(-1, 1)
+        logger.info(f"等待 {this_time-2} 秒，模拟答题中...")
+        time.sleep(this_time - 2)
+        res = self.api.exam_record_question(user_exam_plan_id, question_id, this_time, answers_ids, exam_plan_id)
+        logger.info(f"答题结果：{res}")
+        if res.get("code") != "0":
+            logger.error(f"答题失败，请重新开启考试")
+            return False
+        return True
